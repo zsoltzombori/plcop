993cbb333f3a8d8d229114ce1a4697e39adcfd43
Zsolt Zombori <zsoltizombi@gmail.com>
2020-01-27 03:30:50 +0100

    deleted unnecessary files

--- foreign/xgb.ml
+++
@@ -1,8 +0,0 @@
-type booster;;
-external c_read_model : string -> booster = "read_predictor";;
-external c_predict : booster -> (int * int) list -> float = "xpredict";;
-external c_mem : unit -> int = "x_mem";;
-
-let read_model s = Some (c_read_model s);;
-let predict b l =
-  match b with None -> 1. | Some b -> c_predict b l;;
